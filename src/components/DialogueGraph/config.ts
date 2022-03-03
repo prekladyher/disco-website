ee38bcf70476a0193257ed41bd61602860b41acd
Pavel Horal <pavel.horal@orchitech.cz>
2022-03-03 09:15:40 +0100

    Improve node styling.

--- src/components/DialogueGraph/config.ts
+++ src/components/DialogueGraph/config.ts
@@ -1,4 +1,5 @@
-import type { UserConfigs } from "v-network-graph";
+import { defineConfigs, type Node, type NodeConfig } from "v-network-graph";
+import type { RecursivePartial } from "v-network-graph/lib/common/common";
 
 function getNodeColor(node: any) {
   switch (node.fields?.DialogueEntryType) {
@@ -7,33 +8,63 @@ function getNodeColor(node: any) {
     case "Fork": return "#666666";
     case "Jump": return "#d0c66c";
     case "Instruction": return "#aa3333";
-    default: return "red";
+    default: return "#333333";
   }
 }
 
-export const configs: UserConfigs = {
+const NODE_STYLE: RecursivePartial<NodeConfig> = {
+  normal: {
+    type: (node: Node) => node.fields.DialogueEntryType ? "rect" : "circle",
+    radius: 35,
+    width: 70,
+    height: 70,
+    borderRadius: 4,
+    color: getNodeColor
+  }
+};
+
+export const configs = defineConfigs({
   view: {
     scalingObjects: true,
     fit: true,
   },
   node: {
+    selectable: 1,
     normal: {
-      type: (node) => node.fields.Title === "START" ? "circle" : "rect",
-      radius: 35,
-      width: 70,
-      height: 70,
-      color: getNodeColor,
+      ...NODE_STYLE.normal
+    },
+    selected: {
+      ...NODE_STYLE.normal,
+
     },
     hover: {
-      color: getNodeColor
+      ...NODE_STYLE.normal
+    },
+    focusring: {
+      visible: true,
+      width: 3,
+      padding: 3,
+      color: "#da1a00",
+      dasharray: "4",
     },
     label: {
       fontSize: 15
     }
   },
   edge: {
+    selectable: 1,
     normal: {
-      color: "#888"
+      color: "#888888",
+      width: 2
+    },
+    selected: {
+      color: "#da1a00",
+      animate: true,
+      animationSpeed: 10,
+      width: 3
+    },
+    hover: {
+      color: "#888888"
     },
     marker: {
       target: {
@@ -41,4 +72,4 @@ export const configs: UserConfigs = {
       }
     }
   }
-};
+});
