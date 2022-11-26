16231292a3fe8dbe690d4b8d490429a335470f51
Pavel Horal <pavel.horal@orchitech.cz>
2022-11-26 13:15:59 +0100

    Use color bullets to differentiate resolved strings.

--- src/components/l10n/ResolvedL10n.vue
+++ src/components/l10n/ResolvedL10n.vue
@@ -1,13 +1,58 @@
 <script setup lang="ts">
+import { computed } from "vue";
+
 const props = defineProps<{
   source?: string,
   target?: string
 }>();
+
+const missing = computed(() => {
+  return props.source !== "" && !props.target
+});
 </script>
 
 <template>
   <div>
-    <div>{{source}}</div>
-    <div>{{target}}</div>
+    <div class="source">
+      {{source}}
+  </div>
+    <div
+      class="target"
+      :class=" { missing }"
+      v-if="target !== undefined"
+    >
+      {{target}}
+    </div>
   </div>
 </template>
+
+<style scoped>
+.source::before {
+  content: "⬤";
+  color: #888888;
+  margin-right: 8px;
+}
+
+.source::before {
+  content: "⬤";
+  font-style: normal;
+  color: #888888;
+  margin-right: 8px;
+}
+
+.target {
+  margin-top: 4px;
+}
+
+.target::before {
+  content: "⬤";
+  font-style: normal;
+  color: #008800;
+  margin-right: 8px;
+}
+
+.missing::before {
+  content: "⬤";
+  color: #880000;
+}
+</style>
